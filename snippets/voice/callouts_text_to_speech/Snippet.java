6892e2696e93af8b548c12e547b2a2a4296e08a6
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-05-31 10:31:42 +0200

    refactor: Sync numbers and voice callouts with 'Snippet' templating usage

---
+++ snippets/voice/callouts_text_to_speech/Snippet.java
@@ -0,0 +1,26 @@
+package voice;
+
+import com.sinch.sdk.domains.voice.*;
+import com.sinch.sdk.domains.voice.models.*;
+import com.sinch.sdk.domains.voice.models.requests.*;
+
+public class Snippet {
+
+    public static String execute(VoiceService voiceService) {
+
+        CalloutsService calloutsService = voiceService.callouts();
+
+        String phoneNumber = "YOUR_phone_number";
+        String message =
+                "Hello, this is a call from Sinch. Congratulations! You made your first call.";
+
+        CalloutRequestParametersTTS parameters =
+                CalloutRequestParametersTTS.builder()
+                        .setDestination(DestinationNumber.valueOf(phoneNumber))
+                        .setText(message)
+                        .build();
+
+        String callId = calloutsService.textToSpeech(parameters);
+        return callId;
+    }
+}
