bf42f8ce630e346c331d9f592080198935f4dd86
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-06-20 10:27:18 +0200

    feature (Numbers/RentAndConfig): Add a dedicated update request to edit number's settings

--- snippets/numbers/rent_and_config/Snippet.java
+++ snippets/numbers/rent_and_config/Snippet.java
@@ -1,8 +1,11 @@
 package numbers;
 
+import com.sinch.sdk.domains.numbers.ActiveNumberService;
 import com.sinch.sdk.domains.numbers.AvailableNumberService;
 import com.sinch.sdk.domains.numbers.NumbersService;
 import com.sinch.sdk.domains.numbers.models.ActiveNumber;
+import com.sinch.sdk.domains.numbers.models.requests.ActiveNumberUpdateRequestParameters;
+import com.sinch.sdk.domains.numbers.models.requests.ActiveNumberUpdateVoiceConfigurationRequestParameters;
 import com.sinch.sdk.domains.numbers.models.requests.AvailableNumberRentRequestParameters;
 import com.sinch.sdk.domains.numbers.models.requests.RentSMSConfigurationRequestParameters;
 import java.util.logging.Logger;
@@ -15,19 +18,43 @@ public class Snippet {
 
     AvailableNumberService availableNumbersService = numbersService.available();
 
+    // Available numbers list can be retrieved by using list() function from available service:
+    // See https://developers.sinch.com/quickstart-getting-started-poc/docs/numbers/getting-started/java-sdk/searchavailable
+    String phoneNumber = "available_phone_number_to_be_rented";
     String servicePlanId = "YOUR_service_plan_id";
-    String phoneNumber = "YOUR_phone_number";
+    String appId = "YOUR_app_id";
 
-    ActiveNumber response =
-        availableNumbersService.rent(
-            phoneNumber,
-            AvailableNumberRentRequestParameters.builder()
-                .setSmsConfiguration(
-                    RentSMSConfigurationRequestParameters.builder()
-                        .setServicePlanId(servicePlanId)
-                        .build())
-                .build());
+    String displayName = "Updated from Sinch Java SDK";
+
+    RentSMSConfigurationRequestParameters smsConfiguration =
+        RentSMSConfigurationRequestParameters.builder().setServicePlanId(servicePlanId).build();
+
+    AvailableNumberRentRequestParameters rentRequest =
+        AvailableNumberRentRequestParameters.builder()
+            .setSmsConfiguration(smsConfiguration)
+            .build();
+
+    // rent with configuration settings
+    ActiveNumber response = availableNumbersService.rent(phoneNumber, rentRequest);
 
     LOGGER.info(String.format("Rented number: %s", response));
+
+    // Update rented number to edit configuration:
+    // - adding voice configuration
+    // - setting a display name
+    ActiveNumberService activeNumbersService = numbersService.active();
+
+    ActiveNumberUpdateVoiceConfigurationRequestParameters voiceConfiguration =
+        ActiveNumberUpdateVoiceConfigurationRequestParameters.builder().setAppId(appId).build();
+
+    ActiveNumberUpdateRequestParameters updateRequest =
+        ActiveNumberUpdateRequestParameters.builder()
+            .setDisplayName(displayName)
+            .setVoiceConfiguration(voiceConfiguration)
+            .build();
+
+    response = activeNumbersService.update(phoneNumber, updateRequest);
+
+    LOGGER.info(String.format("Updated number: %s", response));
   }
 }
