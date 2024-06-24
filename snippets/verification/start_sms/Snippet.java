e60e9b84a5e61d40d0901bc1c257867518cb5cef
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2024-06-24 16:24:40 +0200

    refactor: Use versioned domain for verification

--- snippets/verification/start_sms/Snippet.java
+++ snippets/verification/start_sms/Snippet.java
@@ -1,9 +1,10 @@
 package verification;
 
-import com.sinch.sdk.domains.verification.*;
-import com.sinch.sdk.domains.verification.models.*;
-import com.sinch.sdk.domains.verification.models.requests.*;
-import com.sinch.sdk.domains.verification.models.response.*;
+import com.sinch.sdk.domains.verification.api.v1.VerificationService;
+import com.sinch.sdk.domains.verification.api.v1.VerificationStartService;
+import com.sinch.sdk.domains.verification.models.v1.NumberIdentity;
+import com.sinch.sdk.domains.verification.models.v1.start.request.VerificationStartRequestSms;
+import com.sinch.sdk.domains.verification.models.v1.start.response.VerificationStartResponseSms;
 import java.util.logging.Logger;
 
 public class Snippet {
@@ -14,15 +15,15 @@ public class Snippet {
 
     // REMINDER: verification Service require to have set application key/secret
     // onto Sinch Client init
-    VerificationsService verificationsService = verificationService.verifications();
+    VerificationStartService startService = verificationService.verificationStart();
 
     String phoneNumber = "YOUR_phone_number";
 
     LOGGER.info(String.format("Sending a SMS verification to '%s'", phoneNumber));
 
-    StartVerificationResponseSMS response =
-        verificationsService.startSms(
-            StartVerificationSMSRequestParameters.builder()
+    VerificationStartResponseSms response =
+        startService.startSms(
+            VerificationStartRequestSms.builder()
                 .setIdentity(NumberIdentity.valueOf(phoneNumber))
                 .build());
 
