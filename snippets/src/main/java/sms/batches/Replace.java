e2695f42c9d73085c778c68820afd58f9f56d7f2
Jean-Pierre Portier <141755467+JPPortier@users.noreply.github.com>
2025-07-03 11:20:44 +0200

    SMS snippets (#18)

---
+++ snippets/src/main/java/sms/batches/Replace.java
@@ -0,0 +1,53 @@
+/**
+ * Sinch Java Snippet
+ *
+ * <p>This snippet is available at https://github.com/sinch/sinch-sdk-java-snippets
+ *
+ * <p>See https://github.com/sinch/sinch-sdk-java-snippets/blob/main/README.md for details
+ */
+package sms.batches;
+
+import com.sinch.sdk.SinchClient;
+import com.sinch.sdk.domains.sms.api.v1.BatchesService;
+import com.sinch.sdk.domains.sms.models.v1.batches.request.TextRequest;
+import com.sinch.sdk.domains.sms.models.v1.batches.response.BatchResponse;
+import com.sinch.sdk.models.Configuration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+import utils.Settings;
+
+public class Replace {
+
+  private static final Logger LOGGER = Logger.getLogger(Replace.class.getName());
+
+  public static void main(String[] args) {
+
+    String projectId = Settings.getProjectId().orElse("MY_PROJECT_ID");
+    String keyId = Settings.getKeyId().orElse("MY_KEY_ID");
+    String keySecret = Settings.getKeySecret().orElse("MY_KEY_SECRET");
+
+    String batchId = "A_BATCH_ID";
+    List<String> recipients = Arrays.asList("A_NEW_RECIPIENT_PHONE_NUMBER");
+    String body = "A message body updated";
+
+    Configuration configuration =
+        Configuration.builder()
+            .setProjectId(projectId)
+            .setKeyId(keyId)
+            .setKeySecret(keySecret)
+            .build();
+
+    SinchClient client = new SinchClient(configuration);
+
+    BatchesService batchesService = client.sms().v1().batches();
+
+    LOGGER.info(String.format("Replacing batch with ID '%s'", batchId));
+
+    TextRequest request = TextRequest.builder().setTo(recipients).setBody(body).build();
+
+    BatchResponse response = batchesService.replace(batchId, request);
+
+    LOGGER.info("Response: " + response);
+  }
+}
---
+++ snippets/src/main/java/sms/batches/SendDeliveryFeedback.java
@@ -0,0 +1,52 @@
+/**
+ * Sinch Java Snippet
+ *
+ * <p>This snippet is available at https://github.com/sinch/sinch-sdk-java-snippets
+ *
+ * <p>See https://github.com/sinch/sinch-sdk-java-snippets/blob/main/README.md for details
+ */
+package sms.batches;
+
+import com.sinch.sdk.SinchClient;
+import com.sinch.sdk.domains.sms.api.v1.BatchesService;
+import com.sinch.sdk.domains.sms.models.v1.batches.request.SendDeliveryFeedbackRequest;
+import com.sinch.sdk.models.Configuration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+import utils.Settings;
+
+public class SendDeliveryFeedback {
+
+  private static final Logger LOGGER = Logger.getLogger(SendDeliveryFeedback.class.getName());
+
+  public static void main(String[] args) {
+
+    String projectId = Settings.getProjectId().orElse("MY_PROJECT_ID");
+    String keyId = Settings.getKeyId().orElse("MY_KEY_ID");
+    String keySecret = Settings.getKeySecret().orElse("MY_KEY_SECRET");
+
+    String batchId = "A_BATCH_ID";
+    List<String> recipients = Arrays.asList("A_RECIPIENT_PHONE_NUMBER");
+
+    Configuration configuration =
+        Configuration.builder()
+            .setProjectId(projectId)
+            .setKeyId(keyId)
+            .setKeySecret(keySecret)
+            .build();
+
+    SinchClient client = new SinchClient(configuration);
+
+    BatchesService batchesService = client.sms().v1().batches();
+
+    LOGGER.info(String.format("Sending delivery feedback for batch with ID '%s'", batchId));
+
+    SendDeliveryFeedbackRequest request =
+        SendDeliveryFeedbackRequest.builder().setRecipients(recipients).build();
+
+    batchesService.sendDeliveryFeedback(batchId, request);
+
+    LOGGER.info("Done");
+  }
+}
